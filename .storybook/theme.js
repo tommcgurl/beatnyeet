30275663183b5b512c046156ed4fb89482e08b27
Tom McGurl <tom.mcgurl@gmail.com>
2023-01-17 22:40:30 -0500

    Adding custom storybook title

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+
+export default create({
+  brandTitle: 'Beet N Yeet Storybook',
+  brandURL: 'http://localhost:3000',
+  brandImage: '',
+  brandTarget: '_self',
+})
