deff696cd2cd601200462bdbb795c431386ce0a4
Tom McGurl <tom.mcgurl@gmail.com>
2023-01-17 22:25:06 -0500

    Adding Game component and Storybook for testing components.

---
+++ components/Game/Game.tsx
@@ -0,0 +1,19 @@
+import styles from './Game.module.css';
+
+type GameProps = {
+  name: string;
+  coverURL?: string;
+  summary?: string;
+}
+const Game = ({name, coverURL, summary}: GameProps) => (
+  <div className={styles.container}>
+    <img alt={`${name}-cover`} src={coverURL}></img>
+    <div className={styles.infoContainer}>
+      <h3>{name}</h3>
+      <p>{summary}</p>
+    </div>
+  </div>
+)
+
+
+export default Game
